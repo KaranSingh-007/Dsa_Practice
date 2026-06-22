e26c230a9112e51058fe3da9697189b73ff41ac1
KaranSingh-007 <karan7a43@gmail.com>
2026-06-22 22:20:27 +0530

    Code to find no. of digits.

---
+++ NoOfDigit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class NoOfDigit {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int no = sc.nextInt();
+        int count = 0;
+        while(no !=0){
+            no = no/10;
+            count ++;
+        }
+        System.out.println("Total count of digits : "+count);
+    }
+}
