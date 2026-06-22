3a50d4d233d71e8b57c38570bfd7c09db9126ab8
KaranSingh-007 <karan7a43@gmail.com>
2026-06-22 22:12:38 +0530

    Added Prime Number

---
+++ PrimeNo.java
@@ -0,0 +1,18 @@
+// public class PrimeNo{
+//     public static void main(String args[]){
+//         boolean t = true;
+//         for ( int n = 2; n<=100; n++)
+
+//         {
+//         for(int i = 2; i<=n-1; i++){
+//             if(n % i == 0 ){
+//                 t = false;
+//                 break;
+//             }
+//         }
+//         if(t==true ){
+//             System.out.println(n);
+//     }
+// }
+// }
+// }
