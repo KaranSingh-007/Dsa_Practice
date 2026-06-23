502e4feb4338222ad84b2dd62ce209f21da265d7
KaranSingh-007 <karan7a43@gmail.com>
2026-06-23 23:24:59 +0530

    Added Proper Structure.

--- DiffNumbers/BostonNo.java
+++ DiffNumbers/BostonNo.java
@@ -1,24 +1,45 @@
 public class BostonNo {
-    public static void main(String args[]){
-        int num = 378;
-        int n = num;
-        int sum = 0;
-        int div = 2;
-        while(n != 0){
-            int dig = n % 10;
-            sum =  sum + dig;
-            n = n/10; 
-        }
-        n= num;
-        System.out.println(sum);
-        while(n != 1){
-            if(num % div == 0){
-                System.out.println(div+" ");
-                n = n/div;
-            }else{
-                div++;
-            }
+
+    public static void main(String[] args) {
+
+        int n = 378;
+
+        int sumOfDigits = sumOfDigits(n);
+        int primeFactorSum = primeFactorDigitSum(n);
+
+        if(sumOfDigits == primeFactorSum) {
+            System.out.println("Boston Number");
+        } else {
+            System.out.println("Not a Boston Number");
         }
     }
 
+    public static int sumOfDigits(int n) {
+        int sum = 0;
+
+        while(n != 0) {
+            int digit = n % 10;
+            sum += digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+
+    public static int primeFactorDigitSum(int n) {
+        int sum = 0;
+        int div = 2;
+
+        while(n > 1) {
+
+            if(n % div == 0) {
+                sum += sumOfDigits(div);
+                n /= div;
+            } else {
+                div++;
+            }
+        }
+
+        return sum;
+    }
 }
